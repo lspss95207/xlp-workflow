21b0a083385a8f2d363cc12d5b1a82af84c8102c
Samuel Huang <lspss95207@gmail.com>
2020-08-06 12:56:40 +0800

    Add BPMN into presentation area

---
+++ bigbluebutton-html5/imports/ui/components/bpmn/container.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import ErrorBoundary from '/imports/ui/components/error-boundary/component';
+import FallbackPresentation from '/imports/ui/components/fallback-errors/fallback-presentation/component';
+// import PresentationPodService from './service';
+import BpmnDiagram from './component';
+
+// PresentationPods component will be the place to go once we have the presentation pods designs
+// it should give each PresentationAreaContainer some space
+// which it will fill with the uploaded presentation
+
+export default BpmnDiagramContainer = ({ ...props }) => (
+  <ErrorBoundary Fallback={FallbackPresentation}>
+    <BpmnDiagram {...props} />
+  </ErrorBoundary>
+);
+
+// const BpmnDiagramContainer = ({ DiagramWrapperPodIds, ...props }) => {
+//   if (DiagramWrapperIds && DiagramWrapperIds.length > 0) {
+//     return (
+//       <ErrorBoundary Fallback={FallbackPresentation}>
+//         <BpmnDiagram presentationPodIds={DiagramWrapperIds} {...props} />
+//       </ErrorBoundary>
+//     );
+//   }
+
+//   return null;
+// };
+
+// export default withTracker(() => ({
+//   presentationPodIds: PresentationPodService.getPresentationPodIds()
+// }))(PresentationPodsContainer);
+
+// DiagramWrapperContainer.propTypes = {
+//     DiagramWrapperPodIds: PropTypes.arrayOf(PropTypes.shape({
+//     podId: PropTypes.string.isRequired,
+//   })).isRequired,
+// };
